593d051269fbd0141e0960769d1c1e7398ab58e2
JoRobert137 <rjohn.robert@kalvium.community>
2025-08-17 00:45:07 +0530

    Minor changes again

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const songRoutes = require('./routes/songRoutes');
 const fileUpload = require('express-fileupload')
+const compression = require("compression");
+app.use(compression());
 
 dotenv.config();
 const app = express();
@@ -16,6 +18,11 @@ app.use(fileUpload({
   useTempFiles: true,
   tempFileDir: '/tmp/'
 }))
+
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "public, max-age=3600"); // cache 1 hour
+  next();
+});
 
 // Optional: set timeout for all requests
 app.use((req, res, next) => {
